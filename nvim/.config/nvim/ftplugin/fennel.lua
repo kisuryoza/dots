5585a41f21b9d5ab862af31623eac1f58a7ada80
justAlex0 <edimmu@disroot.org>
2023-01-22 11:24:29 +0400

    feat(nvim): ftplugins

---
+++ nvim/.config/nvim/ftplugin/fennel.lua
@@ -0,0 +1,5 @@
+local opt_l = vim.opt_local
+opt_l.formatprg = "fnlfmt -"
+opt_l.tabstop = 2
+opt_l.shiftwidth = 2
+opt_l.softtabstop = 2
---
+++ nvim/.config/nvim/ftplugin/sh.lua
@@ -0,0 +1,2 @@
+local opt_l = vim.opt_local
+opt_l.formatprg = "shfmt -i 4"
